3811a7408a597a5ef96c618d9d7749336beb2ff6
Aleksandr Arofikin <sashaaro@gmail.com>
2017-08-02 08:14:22 +0300

    install and integration pug templating engine

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const slackClient = require('@slack/client')
 const express = require('express')
 const session = require('express-session')
 const request = require('request-promise')
+const pug = require('pug');
 
 
 const MongoClient = mongodb.MongoClient
@@ -70,8 +71,7 @@ app.get('/', async (req, res) => {
 
     return res.redirect('/dashboard')
   }
-  const response = '<a href="https://slack.com/oauth/authorize?&client_id=220827250899.220366847441&scope=bot,channels:history,team:read"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>';
-  res.send(response)
+  res.send(pug.compileFile('templates/index.pug')({}))
 })
 
 app.get('/dashboard', async (req, res) => {
