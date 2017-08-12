b61bcc59a305768380550e7260f151f893ab65ef
Aleksandr Arofikin <sashaaro@gmail.com>
2017-08-12 17:00:11 +0300

    settings

--- server/index.js
+++ server/index.js
@@ -115,84 +115,53 @@ app.get('/dashboard/settings', async (req, res) => {
     }
     const channels = response.channels
 
+  const timezoneList = {
+    '-12.0': '(GMT -12:00) Eniwetok, Kwajalein',
+    '-11.0': '(GMT -11:00) Midway Island, Samoa',
+    '-10.0': '(GMT -10:00) Hawaii',
+    '-9.0': '(GMT -9:00) Alaska',
+    '-8.0': '(GMT -8:00) Pacific Time (US &amp; Canada)',
+    '-7.0': '(GMT -7:00) Mountain Time (US &amp; Canada)',
+    '-6.0': '(GMT -6:00) Central Time (US &amp; Canada), Mexico City',
+    '-5.0': '(GMT -5:00) Eastern Time (US &amp; Canada), Bogota, Lima',
+    '-4.0': '(GMT -4:00) Atlantic Time (Canada), Caracas, La Paz',
+    '-3.5': '(GMT -3:30) Newfoundland',
+    '-3.0': '(GMT -3:00) Brazil, Buenos Aires, Georgetown',
+    '-2.0': '(GMT -2:00) Mid-Atlantic',
+    '-1.0': '(GMT -1:00 hour) Azores, Cape Verde Islands',
+    '0.0': '(GMT) Western Europe Time, London, Lisbon, Casablanca',
+    '1.0': '(GMT +1:00 hour) Brussels, Copenhagen, Madrid, Paris',
+    '2.0': '(GMT +2:00) Kaliningrad, South Africa',
+    '3.0': '(GMT +3:00) Baghdad, Riyadh, Moscow, St. Petersburg',
+    '3.5': '(GMT +3:30) Tehran',
+    '4.0': '(GMT +4:00) Abu Dhabi, Muscat, Baku, Tbilisi',
+    '4.5': '(GMT +4:30) Kabul',
+    '5.0': '(GMT +5:00) Ekaterinburg, Islamabad, Karachi, Tashkent',
+    '5.5': '(GMT +5:30) Bombay, Calcutta, Madras, New Delhi',
+    '5.75': '(GMT +5:45) Kathmandu',
+    '6.0': '(GMT +6:00) Almaty, Dhaka, Colombo',
+    '7.0': '(GMT +7:00) Bangkok, Hanoi, Jakarta',
+    '8.0': '(GMT +8:00) Beijing, Perth, Singapore, Hong Kong',
+    '9.0': '(GMT +9:00) Tokyo, Seoul, Osaka, Sapporo, Yakutsk',
+    '9.5': '(GMT +9:30) Adelaide, Darwin',
+    '10.0': '(GMT +10:00) Eastern Australia, Guam, Vladivostok',
+    '11.0': '(GMT +11:00) Magadan, Solomon Islands, New Caledonia',
+    '12.0': '(GMT +12:00) Auckland, Wellington, Fiji, Kamchatka',
+  };
 
-  /*
+  const settings = {
+    timezone: '0.0',
+    start: '10:00',
+    end: '10:30'
+  }
 
-   <option value="-12.0">(GMT -12:00) Eniwetok, Kwajalein</option>
-   <option value="-11.0">(GMT -11:00) Midway Island, Samoa</option>
-   <option value="-10.0">(GMT -10:00) Hawaii</option>
-   <option value="-9.0">(GMT -9:00) Alaska</option>
-   <option value="-8.0">(GMT -8:00) Pacific Time (US &amp; Canada)</option>
-   <option value="-7.0">(GMT -7:00) Mountain Time (US &amp; Canada)</option>
-   <option value="-6.0">(GMT -6:00) Central Time (US &amp; Canada), Mexico City</option>
-   <option value="-5.0">(GMT -5:00) Eastern Time (US &amp; Canada), Bogota, Lima</option>
-   <option value="-4.0">(GMT -4:00) Atlantic Time (Canada), Caracas, La Paz</option>
-   <option value="-3.5">(GMT -3:30) Newfoundland</option>
-   <option value="-3.0">(GMT -3:00) Brazil, Buenos Aires, Georgetown</option>
-   <option value="-2.0">(GMT -2:00) Mid-Atlantic</option>
-   <option value="-1.0">(GMT -1:00 hour) Azores, Cape Verde Islands</option>
-   <option value="0.0">(GMT) Western Europe Time, London, Lisbon, Casablanca</option>
-   <option value="1.0">(GMT +1:00 hour) Brussels, Copenhagen, Madrid, Paris</option>
-   <option value="2.0">(GMT +2:00) Kaliningrad, South Africa</option>
-   <option value="3.0">(GMT +3:00) Baghdad, Riyadh, Moscow, St. Petersburg</option>
-   <option value="3.5">(GMT +3:30) Tehran</option>
-   <option value="4.0">(GMT +4:00) Abu Dhabi, Muscat, Baku, Tbilisi</option>
-   <option value="4.5">(GMT +4:30) Kabul</option>
-   <option value="5.0">(GMT +5:00) Ekaterinburg, Islamabad, Karachi, Tashkent</option>
-   <option value="5.5">(GMT +5:30) Bombay, Calcutta, Madras, New Delhi</option>
-   <option value="5.75">(GMT +5:45) Kathmandu</option>
-   <option value="6.0">(GMT +6:00) Almaty, Dhaka, Colombo</option>
-   <option value="7.0">(GMT +7:00) Bangkok, Hanoi, Jakarta</option>
-   <option value="8.0">(GMT +8:00) Beijing, Perth, Singapore, Hong Kong</option>
-   <option value="9.0">(GMT +9:00) Tokyo, Seoul, Osaka, Sapporo, Yakutsk</option>
-   <option value="9.5">(GMT +9:30) Adelaide, Darwin</option>
-   <option value="10.0">(GMT +10:00) Eastern Australia, Guam, Vladivostok</option>
-   <option value="11.0">(GMT +11:00) Magadan, Solomon Islands, New Caledonia</option>
-   <option value="12.0">(GMT +12:00) Auckland, Wellington, Fiji, Kamchatka</option>
-   */
-  const timezoneList = [
-   '(GMT -12:00) Eniwetok, Kwajalein',
-  '(GMT -11:00) Midway Island, Samoa',
-  '(GMT -10:00) Hawaii',
-  '(GMT -9:00) Alaska',
-  '(GMT -8:00) Pacific Time (US &amp; Canada)',
-  '(GMT -7:00) Mountain Time (US &amp; Canada)',
-  '(GMT -6:00) Central Time (US &amp; Canada), Mexico City',
-  '(GMT -5:00) Eastern Time (US &amp; Canada), Bogota, Lima',
-  '(GMT -4:00) Atlantic Time (Canada), Caracas, La Paz',
-  '(GMT -3:30) Newfoundland',
-  '(GMT -3:00) Brazil, Buenos Aires, Georgetown',
-  '(GMT -2:00) Mid-Atlantic',
-  '(GMT -1:00 hour) Azores, Cape Verde Islands',
-  '(GMT) Western Europe Time, London, Lisbon, Casablanca',
-  '(GMT +1:00 hour) Brussels, Copenhagen, Madrid, Paris',
-  '(GMT +2:00) Kaliningrad, South Africa',
-  '(GMT +3:00) Baghdad, Riyadh, Moscow, St. Petersburg',
-  '(GMT +3:30) Tehran',
-  '(GMT +4:00) Abu Dhabi, Muscat, Baku, Tbilisi',
-  '(GMT +4:30) Kabul',
-  '(GMT +5:00) Ekaterinburg, Islamabad, Karachi, Tashkent',
-  '(GMT +5:30) Bombay, Calcutta, Madras, New Delhi',
-  '(GMT +5:45) Kathmandu',
-  '(GMT +6:00) Almaty, Dhaka, Colombo',
-  '(GMT +7:00) Bangkok, Hanoi, Jakarta',
-  '(GMT +8:00) Beijing, Perth, Singapore, Hong Kong',
-  '(GMT +9:00) Tokyo, Seoul, Osaka, Sapporo, Yakutsk',
-  '(GMT +9:30) Adelaide, Darwin',
-  '(GMT +10:00) Eastern Australia, Guam, Vladivostok',
-  '(GMT +11:00) Magadan, Solomon Islands, New Caledonia',
-  '(GMT +12:00) Auckland, Wellington, Fiji, Kamchatka',
-  ];
-
-
-    res.send(pug.compileFile('templates/dashboard/settings.pug')({
-        team: user.team_name,
-        authLink: authLink,
-        channels,
-        timezoneList
-    }))
-
-    //res.send(`Team ${user.team_name} dashboard`)
+  res.send(pug.compileFile('templates/dashboard/settings.pug')({
+    team: user.team_name,
+    authLink: authLink,
+    channels,
+    timezoneList,
+    settings
+  }))
 })
 
 
