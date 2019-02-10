d2b63bb2f328ef837777f558c3e951d4180c84a4
Aleksandr <sashaaro@gmail.com>
2019-02-10 15:04:07 +0300

    update scope

--- server/http/controller/auth.ts
+++ server/http/controller/auth.ts
@@ -22,7 +22,8 @@ export class AuthAction implements IHttpAction {
 
     const redirectUri = `${this.config.host}/auth`
     if (!req.query.code) {
-      const authLink = `https://slack.com/oauth/authorize?&client_id=${this.config.slackClientID}&scope=bot,channels:read,team:read,groups:read&redirect_uri=${redirectUri}`
+      const scopes = ['bot', 'channels:read', 'team:read', 'groups:read', 'im:history']
+      const authLink = `https://slack.com/oauth/authorize?&client_id=${this.config.slackClientID}&scope=${scopes.join(',')}&redirect_uri=${redirectUri}`
 
       res.send(pug.compileFile(`${templateDirPath}/auth.pug`)({
         authLink: authLink
