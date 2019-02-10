529cc1b58fcee8069d8713f1638861b947ebaca5
Aleksandr <sashaaro@gmail.com>
2019-02-10 15:37:44 +0300

    listen message.app_home

--- server/slack/SlackStandUpProvider.ts
+++ server/slack/SlackStandUpProvider.ts
@@ -89,7 +89,8 @@ export class SlackStandUpProvider implements IStandUpProvider, ITransport {
     this.message$ = Observable.create((observer) => {
       const userRepository = this.connection.getRepository(User);
 
-      this.rtm.on('message.app_home event', async (messageResponse: RTMMessageResponse) => {
+      this.rtm.on('message.app_home', async (messageResponse: RTMMessageResponse) => {
+        console.log(messageResponse)
         // be sure it is direct answerMessage to bot
         if (!userRepository.findOne({where: {im: messageResponse.channel}})) {
           console.log(`User channel ${messageResponse.channel} is not im`);
