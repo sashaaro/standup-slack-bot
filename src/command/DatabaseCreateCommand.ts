12ce0bc9faf23d1483c20a279572527c52314355
Aleksandr Arofikin <sashaaro@gmail.com>
2020-05-02 11:45:41 +0300

    fix database commands

--- src/command/DatabaseCreateCommand.ts
+++ src/command/DatabaseCreateCommand.ts
@@ -1,7 +1,8 @@
 import * as yargs from "yargs";
-import {createProviders} from "../services/providers";
+import {createProviders, IAppConfig} from "../services/providers";
 import {ReflectiveInjector} from "injection-js";
 import {Connection} from "typeorm";
+import {CONFIG_TOKEN} from "../services/token";
 
 export class DatabaseCreateCommand implements yargs.CommandModule {
   command = 'database:create';
@@ -13,7 +14,22 @@ export class DatabaseCreateCommand implements yargs.CommandModule {
     const connection = await injector.get(Connection) as Connection;
     await connection.connect();
 
-    const migrations = await connection.runMigrations();
-    migrations.map(m => m.name).map(console.log)
+    const config: IAppConfig = injector.get(CONFIG_TOKEN)
+    const db = config.db;
+
+    const isDatabaseExist = ((await connection.query(`SELECT datname FROM pg_database WHERE datname = '${db.database}' LIMIT 1`)) as any[]).length === 1;
+    if (!isDatabaseExist) {
+      await connection.query(`CREATE DATABASE ${db.database}`);
+
+      console.log(`Database ${db.database} was created`);
+    }
+
+    const isUserExist = ((await connection.query(`SELECT 1 FROM pg_roles WHERE rolname='${db.username}' LIMIT 1`)) as any[]).length === 1;
+    if (!isUserExist) {
+      await connection.query(`CREATE USER ${db.username} WITH PASSWORD '${db.password}'`);
+      await connection.query(`GRANT ALL PRIVILEGES ON DATABASE ${db.database} TO ${db.username}`);
+
+      console.log(`User ${db.username} was created`);
+    }
   }
 }
--- src/command/DatabaseMigrateCommand.ts
+++ src/command/DatabaseMigrateCommand.ts
@@ -1,6 +1,5 @@
 import * as yargs from "yargs";
-import {createProviders, IAppConfig} from "../services/providers";
-import {CONFIG_TOKEN} from "../services/token";
+import {createProviders} from "../services/providers";
 import {ReflectiveInjector} from "injection-js";
 import {Connection} from "typeorm";
 
@@ -14,22 +13,7 @@ export class DatabaseMigrateCommand implements yargs.CommandModule {
     const connection = await injector.get(Connection) as Connection;
     await connection.connect();
 
-    const config: IAppConfig = injector.get(CONFIG_TOKEN)
-    const db = config.db;
-
-    const isDatabaseExist = ((await connection.query(`SELECT datname FROM pg_database WHERE datname = '${db.database}' LIMIT 1`)) as any[]).length === 1;
-    if (!isDatabaseExist) {
-      await connection.query(`CREATE DATABASE ${db.database}`);
-
-      console.log(`Database ${db.database} was created`);
-    }
-
-    const isUserExist = ((await connection.query(`SELECT 1 FROM pg_roles WHERE rolname='${db.username}' LIMIT 1`)) as any[]).length === 1;
-    if (!isUserExist) {
-      await connection.query(`CREATE USER ${db.username} WITH PASSWORD '${db.password}'`);
-      await connection.query(`GRANT ALL PRIVILEGES ON DATABASE ${db.database} TO ${db.username}`);
-
-      console.log(`User ${db.username} was created`);
-    }
+    const migrations = await connection.runMigrations();
+    migrations.map(m => m.name).map(console.log)
   }
 }
